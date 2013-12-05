7c73024e335b8e5a2a347df722921bdd4d24d9ec
Will Mernagh <wmernagh@gmail.com>
2013-12-05 14:05:25 +0100

    Read in settings from env

--- README.md
+++ README.md
@@ -3,10 +3,10 @@ WIP - result will output open pulls something like:
 ```
 Open Pulls
 
-| Snowflake | Comments | Passing | :octocatted: |
-| #199      | 3        |   ✓     |     ✓        |
-| #201      | 1        |   x     |     x        |
-| #198      | 0        |   ✓     |     x        |
+| Snowflake | Comments | Passing | :octocatted: | created_by | last_comment_by | title |
+| #199      | 3        | ✓       | ✓            |
+| #201      | 1        | x       | x            |
+| #198      | 0        | ✓       | x            |
 ```
 
 CLI App
--- gogit.go
+++ gogit.go
@@ -1,25 +1,48 @@
 package gogit
 
 import (
+	"syscall"
 	"fmt"
 	"code.google.com/p/goauth2/oauth"
 	"github.com/google/go-github/github"
 )
 
-var t = &oauth.Transport{
-	Token: &oauth.Token{AccessToken: "7c5f06367ffea77071c84e32f02a505304248097"},
-}
-var client = github.NewClient(t.Client())
+var t *oauth.Transport
+var client *github.Client
 
 func Run() {
-  repo := Repo{"IoraHealth", "IoraHealth"}
-  pulls, _ := repo.OpenPulls()
+	opts, ok := readOptions()
 
-  for _, pull := range pulls {
-	  fmt.Printf("[number: %d, comments: %d, status: %s, octocatted: %v]\n",
-	             pull.State.Number,
-	             pull.State.CommentCount,
-	             pull.State.Status,
-	             pull.State.Octocatted)
-  }
+	if !ok {
+		fmt.Printf("Please set your env variables\n")
+		fmt.Printf("GOGIT_GH_TOKEN, GOGIT_GH_OWNER, GOGIT_GH_REPO\n")
+		return
+	}
+
+	t = &oauth.Transport{ Token: &oauth.Token{AccessToken: opts["TOKEN"]} }
+	client = github.NewClient(t.Client())
+
+	repo := Repo{opts["OWNER"], opts["REPO"]}
+	pulls, _ := repo.OpenPulls()
+
+	for _, pull := range pulls {
+		fmt.Printf("[number: %d, comments: %d, status: %s, octocatted: %v]\n",
+		pull.State.Number,
+		pull.State.CommentCount,
+		pull.State.Status,
+		pull.State.Octocatted)
+	}
+}
+
+func readOptions() (opts map[string]string, ok bool) {
+	accessToken, okToken := syscall.Getenv("GOGIT_GH_TOKEN")
+	owner, okOwner := syscall.Getenv("GOGIT_GH_OWNER")
+	repoName, okRepo := syscall.Getenv("GOGIT_GH_REPO")
+
+	opts = map[string]string {
+		"TOKEN": accessToken,
+		"OWNER": owner,
+		"REPO": repoName,
+	}
+	return opts, (okToken && okOwner && okRepo)
 }
